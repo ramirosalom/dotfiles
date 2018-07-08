bc204af7db1a07733987d81bdffcc49890e244ae
Cristian Stügelmayer <cristian@yellowspot.com.ar>
2018-07-08 15:01:00 -0300

    Move zsh-git-prompt to custom folder

--- zsh-git-prompt.plugin.zsh
+++ zsh-git-prompt.plugin.zsh
@@ -1,3 +1,3 @@
-source ~/.oh-my-zsh/plugins/zsh-git-prompt/zshrc.zsh
+source ~/.oh-my-zsh/custom/plugins/zsh-git-prompt/zshrc.zsh
 # an example prompt
 PROMPT='%B%m@%{$fg[blue]%}$(rvm-prompt)%{$fg[white]%}%~%b$(git_super_status) %# '
